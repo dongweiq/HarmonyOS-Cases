ff98e2353d3fd493868bc91affe8f02e8c87f6b4
chenyongjian <chenyongjian10@huawei.com>
2024-03-25 08:48:49 +0000

    !328 【bugfix】修改《听歌识曲水波纹特效案例》扫描问题 * 【feature】修改《听歌识曲水波纹特效案例》扫描问题

--- CommonAppDevelopment/feature/waterripples/hvigorfile.ts
+++ CommonAppDevelopment/feature/waterripples/hvigorfile.ts
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+"use strict";
 
 import { harTasks } from '@ohos/hvigor-ohos-plugin';
 
